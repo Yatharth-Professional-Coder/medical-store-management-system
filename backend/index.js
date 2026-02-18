2a19e237aafd6c86c1fec5ee096c82dfc7fc5f21
Yatharth <yatharth@Yatharths-MacBook-Air.local>
2026-02-18 12:07:20 +0530

    Fix: Connect to MongoDB in index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 
+const connectDB = require('./config/db');
+
 dotenv.config();
+connectDB();
 
 const app = express();
 
